6e31ca816e527c9ae85679c51f9e956d74cc6d41
Anastasia <anastasiazavrak@gmail.com>
2022-06-11 20:15:33 +0100

    Monsters with movements and rocket with shooting added.

--- src/Tester.java
+++ src/Tester.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.util.ArrayList;
 
 public class Tester {
@@ -229,27 +228,43 @@ class MainScreen {
 }
 
 class GameScreen {
-    private final int WINDOW_WIDTH = 1200;
+    private final int WINDOW_WIDTH = 1400;
     private final int WINDOW_HEIGHT = 800;
+    private final int SHOOT_DELAY_BETWEEN_FRAMES = 3;
 
     // Icons
     private final ImageIcon background = new ImageIcon("images/background.png"),
-            livesIco =  new ImageIcon("images/labels/lives.png"),
-            scoreIco =  new ImageIcon("images/labels/score.png"),
-            levelIco =  new ImageIcon("images/labels/level.png"),
+            livesIco = new ImageIcon("images/labels/lives.png"),
+            scoreIco = new ImageIcon("images/labels/score.png"),
+            levelIco = new ImageIcon("images/labels/level.png"),
             fullHeartIco = new ImageIcon("images/icons/full_heart.png"),
             emptyHeartIco = new ImageIcon("images/icons/empty_heart.png"),
-            gameOverIco =  new ImageIcon("images/labels/game_over.png");
+            rocketIco = new ImageIcon("images/icons/rocket.png"),
+            rocketShotIco = new ImageIcon("images/icons/rocket_shot.png"),
+            gameOverIco = new ImageIcon("images/labels/game_over.png"),
+            monster10Ico = new ImageIcon("images/monsters/monster1_0.png"),
+            monster11Ico = new ImageIcon("images/monsters/monster1_1.png"),
+            monster20Ico = new ImageIcon("images/monsters/monster2_0.png"),
+            monster21Ico = new ImageIcon("images/monsters/monster2_1.png"),
+            monster30Ico = new ImageIcon("images/monsters/monster3_0.png"),
+            monster31Ico = new ImageIcon("images/monsters/monster3_1.png"),
+            monster40Ico = new ImageIcon("images/monsters/monster4_0.png"),
+            monster41Ico = new ImageIcon("images/monsters/monster4_1.png"),
+            monster50Ico = new ImageIcon("images/monsters/monster5_0.png"),
+            monster51Ico = new ImageIcon("images/monsters/monster5_1.png");
 
     // Screen Elements
     private final JLabel livesLabel = new JLabel(livesIco),
             scoreLabel = new JLabel(scoreIco),
             levelLabel = new JLabel(levelIco),
+            rocketLabel = new JLabel(rocketIco),
+            rocketShotLabel = new JLabel(rocketShotIco),
             gameOverLabel = new JLabel(gameOverIco);
     private final ArrayList<JLabel> hearts = new ArrayList<>(),
             numbers = new ArrayList<>(),
             levelScreenNumbers = new ArrayList<>(),
-            scoreScreenNumbers = new ArrayList<>();
+            scoreScreenNumbers = new ArrayList<>(),
+            monsters = new ArrayList<>();
 
 
 
@@ -257,23 +272,59 @@ class GameScreen {
     private final JFrame frame = new JFrame();
     private final ImagePanel panel = new ImagePanel(background.getImage());
 
+    private boolean rocketIsOut = false;
+
     public GameScreen() {
         setPanel();
         setFrame();
         setVisible();
-        new Timer(2000, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ((Timer) e.getSource()).stop();
-                setLevel(12300, 10);
-                setScore(12500, 10);
-            }
-        }).start();
     }
 
     public void setVisible() {
         frame.setVisible(true);
     }
+
+    private void setFrame() {
+        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setTitle("Space Defenders");
+        frame.addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {}
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                if (e.getX() > rocketIco.getIconWidth()/2 && e.getX() < WINDOW_WIDTH - rocketIco.getIconWidth()/2) {
+                    rocketLabel.setBounds(e.getX() - rocketIco.getIconWidth()/2, 650, rocketIco.getIconWidth(), rocketIco.getIconHeight());
+                }
+            }
+        });
+
+        frame.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                shoot();
+            }
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+        frame.add(panel);
+    }
+
 
     private void setPanel() {
         panel.setLayout(null);
@@ -299,6 +350,97 @@ class GameScreen {
         setScore(0, 10);
         panel.add(levelLabel);
         setLevel(1, 10);
+
+        rocketLabel.setBounds(WINDOW_WIDTH/2 - rocketIco.getIconWidth()/2, 650, rocketIco.getIconWidth(), rocketIco.getIconHeight());
+
+        panel.add(rocketLabel);
+
+        createMonsters();
+
+        for (JLabel monster : monsters) {
+            panel.add(monster);
+        }
+
+        var movementDist = 20;
+        var timeInterval = 1000;
+        animateMonsters(movementDist, timeInterval);
+
+    }
+
+    private boolean right = true;
+
+    private void animateMonsters(int movementDist, int timeInterval) {
+        var width = monster10Ico.getIconWidth();
+        new Timer(timeInterval, e -> {
+            if (right) {
+                if (collisionCheck(width)) {
+                    right = false;
+                    moveLeft(movementDist, timeInterval);
+                } else {
+                    moveRight(movementDist, timeInterval);
+                }
+            } else {
+                if (collisionCheck(width)) {
+                    right = true;
+                    moveRight(movementDist, timeInterval);
+                } else {
+                    moveLeft(movementDist, timeInterval);
+                }
+            }
+        }).start();
+        new Timer(timeInterval, e1 -> {
+            for (JLabel monster : monsters) {
+                Icon icon = monster.getIcon();
+                if (monster10Ico.equals(icon)) {
+                    monster.setIcon(monster11Ico);
+                } else if (monster20Ico.equals(icon)) {
+                    monster.setIcon(monster21Ico);
+                } else if (monster30Ico.equals(icon)) {
+                    monster.setIcon(monster31Ico);
+                } else if (monster40Ico.equals(icon)) {
+                    monster.setIcon(monster41Ico);
+                } else if (monster50Ico.equals(icon)) {
+                    monster.setIcon(monster51Ico);
+                } else if (monster11Ico.equals(icon)) {
+                    monster.setIcon(monster10Ico);
+                } else if (monster21Ico.equals(icon)) {
+                    monster.setIcon(monster20Ico);
+                } else if (monster31Ico.equals(icon)) {
+                    monster.setIcon(monster30Ico);
+                } else if (monster41Ico.equals(icon)) {
+                    monster.setIcon(monster40Ico);
+                } else if (monster51Ico.equals(icon)) {
+                    monster.setIcon(monster50Ico);
+                }
+            }
+        }).start();
+    }
+    private void createMonsters() {
+        var distY = 10;
+        var y = 100;
+        var monsterHeight = monster10Ico.getIconHeight();
+        createMonsterRaw(monster10Ico, y);
+        y += distY + monsterHeight;
+        createMonsterRaw(monster20Ico, y);
+        y += distY + monsterHeight;
+        createMonsterRaw(monster30Ico, y);
+        y += distY + monsterHeight;
+        createMonsterRaw(monster40Ico, y);
+        y += distY + monsterHeight;
+        createMonsterRaw(monster50Ico, y);
+    }
+    private void createMonsterRaw(ImageIcon ico, int y) {
+        var distX = 40;
+        var monsterWidth = monster10Ico.getIconWidth();
+        var monsterHeight = monster10Ico.getIconHeight();
+
+        for (int i = 0; i < 11; i++) {
+            monsters.add(new JLabel(ico));
+            monsters.get(monsters.size()-1).setBounds(
+                    WINDOW_WIDTH/2 - monsterWidth/2 - 5* distX - 5*monsterWidth + i*(monsterWidth + distX), y,
+                    monsterWidth, monsterHeight
+            );
+        }
     }
 
     private void setLevel(int level, int y) {
@@ -325,7 +467,6 @@ class GameScreen {
         }
         panel.repaint();
     }
-
     private void setScore(int score, int y) {
         for (JLabel lab : scoreScreenNumbers) {
             panel.remove(lab);
@@ -350,7 +491,6 @@ class GameScreen {
         }
         panel.repaint();
     }
-
     private void setLives(int lives) {
         var empty = 3 - lives;
         for (int i = 0; i < lives; i++) {
@@ -361,12 +501,92 @@ class GameScreen {
         }
     }
 
-    private void setFrame() {
-        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setTitle("Space Defenders");
-        frame.add(panel);
+    private void shoot() {
+        if (!rocketIsOut) {
+            rocketShotLabel.setBounds(rocketLabel.getX() + rocketIco.getIconWidth() / 2 - rocketShotIco.getIconWidth() / 2, rocketLabel.getY() + 15, rocketShotIco.getIconWidth(), rocketShotIco.getIconHeight());
+            panel.add(rocketShotLabel);
+            rocketIsOut = true;
+            animateShoot(rocketShotLabel, new Point(rocketShotLabel.getX(), - rocketShotIco.getIconHeight()));
+        }
+    }
+    private void animateShoot(JComponent component, Point newPoint) {
+        Rectangle compBounds = component.getBounds();
+        var frames = 350;
+
+        Point oldPoint = new Point(compBounds.x, compBounds.y),
+                animFrame = new Point((newPoint.x - oldPoint.x) / frames,
+                        (newPoint.y - oldPoint.y) / frames);
+
+        new Timer(SHOOT_DELAY_BETWEEN_FRAMES, new ActionListener() {
+            int currentFrame = 0;
+            public void actionPerformed(ActionEvent e) {
+                component.setBounds(oldPoint.x + (animFrame.x * currentFrame),
+                        oldPoint.y + (animFrame.y * currentFrame),
+                        compBounds.width,
+                        compBounds.height);
+
+                if (currentFrame != frames)
+                    currentFrame++;
+                else {
+                    rocketIsOut = false;
+                    ((Timer) e.getSource()).stop();
+                }
+            }
+        }).start();
+    }
+
+    private boolean collisionCheck(int width) {
+        var min = WINDOW_WIDTH;
+        var max = 0;
+        for (JLabel monster : monsters) {
+            if (monster.getX() > max) {
+                max = monster.getX();
+            }
+            if (monster.getX() < min) {
+                min = monster.getX();
+            }
+        }
+        if (max + width + width/2 >= WINDOW_WIDTH || min - width/2 <= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    private void moveRight(int movementDist, int timeInterval) {
+        for (JLabel monster : monsters) {
+            animate(monster, new Point(monster.getX() + movementDist, monster.getY()), movementDist, timeInterval/2/movementDist);
+        }
+    }
+
+    private void moveLeft(int movementDist, int timeInterval) {
+        for (JLabel monster : monsters) {
+            animate(monster, new Point(monster.getX() - movementDist, monster.getY()), movementDist, timeInterval/2/movementDist);
+        }
+    }
+
+    private void cycledAnimate(JComponent component, Point newPoint, int frames, int interval) {
+        Rectangle compBounds = component.getBounds();
+
+        Point oldPoint = new Point(compBounds.x, compBounds.y),
+                animFrame = new Point((newPoint.x - oldPoint.x) / frames,
+                        (newPoint.y - oldPoint.y) / frames);
+
+        new Timer(interval, new ActionListener() {
+            int currentFrame = 0;
+            public void actionPerformed(ActionEvent e) {
+                component.setBounds(oldPoint.x + (animFrame.x * currentFrame),
+                        oldPoint.y + (animFrame.y * currentFrame),
+                        compBounds.width,
+                        compBounds.height);
+
+                if (currentFrame != frames) {
+                    currentFrame++;
+                } else {
+                    ((Timer) e.getSource()).stop();
+                    cycledAnimate(component, oldPoint, frames, interval);
+                }
+            }
+        }).start();
     }
 
     private void animate(JComponent component, Point newPoint, int frames, int interval) {
